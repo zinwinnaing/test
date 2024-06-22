cf1fa7e11e45cd99797dcd5e67caba1551c8253d
zinwinnaing <zinwinnaing1996820@gmail.com>
2024-06-22 17:44:10 +0630

    fixed in service

--- src/service/weatherService.tsx
+++ src/service/weatherService.tsx
@@ -1,36 +1,39 @@
-// src/services/weatherService.ts
-import axios from 'axios';
-import { Weather, Forecast ,City} from '../types';
-
+import { Weather, Forecast, City } from '../types';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = process.env.REACT_APP_WEATHER_API_URL;
 
+// Ensure the base URL uses HTTPS
+const SECURE_BASE_URL = BASE_URL?.replace('http://', 'https://');
+
 export const getCurrentWeather = async (city: City): Promise<Weather> => {
-    
-  const response = await axios.get(`${BASE_URL}/weather?lat=${city?.latitude}&lon=${city?.longitude}&appid=${API_KEY}`);
-  const data = response.data;
-  return {
-    temperature: data.main.temp,
-    description: data.weather[0].description,
-    icon: data.weather[0].icon,
-    city: data.name,
-    country: data.sys.country,
-  };
+  try {
+    const response = await fetch(`${SECURE_BASE_URL}/weather?lat=${city.latitude}&lon=${city.longitude}&appid=${API_KEY}&units=metric`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return {
+      temperature: data.main.temp,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
+      city: data.name,
+      country: data.sys.country,
+    };
+  } catch (error) {
+    console.error('Error fetching current weather:', error);
+    throw error;
+  }
 };
 
 export const getWeatherForecast = async (city: City): Promise<Forecast[]> => {
   try {
-    const response = await axios.get(`${BASE_URL}/forecast`, {
-      params: {
-        lat: city.latitude,
-        lon: city.longitude,
-        appid: API_KEY,
-        units: 'metric'
-      }
-    });
-
-    return response.data.list.map((item: any) => ({
+    const response = await fetch(`${SECURE_BASE_URL}/forecast?lat=${city.latitude}&lon=${city.longitude}&appid=${API_KEY}&units=metric`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data.list.map((item: any) => ({
       date: item.dt_txt,
       temperature: item.main.temp,
       description: item.weather[0].description,
